dc6ecf4870545a385189ac2bfb314146403ad19d
Sandeep Yadav <163108963+Sandy-7824@users.noreply.github.com>
2024-03-13 15:49:33 +0530

    Add files via upload

---
+++ vegetable site/vegetable site/veg 2.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("march 31, 2024 00:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var day = Math.floor(distance / (1000 * 60 *60 *24));
+    var hour = Math.floor((distance % (1000 * 60 *60 *24)) / (1000 *60 *60));
+    var minute = Math.floor((distance % (1000 * 60 *60)) / (1000 *60));
+    var second = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("day").innerHTML = day;
+    document.getElementById("hour").innerHTML = hour;
+    document.getElementById("minute").innerHTML = minute;
+    document.getElementById("second").innerHTML = second;
+
+    
+},1000);
